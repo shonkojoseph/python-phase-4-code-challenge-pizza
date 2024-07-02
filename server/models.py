ec3fa8e54592c9389d789bc79824a5f2a800de99
Joseph <josephshonko99@gmail.com>
2024-07-02 11:24:48 +0300

    commiting migration and server

--- server/models.py
+++ server/models.py
@@ -21,8 +21,12 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship(
+        "RestaurantPizza",
+        back_populates="restaurant",  
+    )
     # add serialization rules
+    serialize_rules = ['-restaurant_pizzas.restaurant']
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +40,13 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
-
+    restaurant_pizzas = db.relationship(
+        "RestaurantPizza",
+        back_populates="pizza",  
+    )
     # add serialization rules
+    serialize_rules = ['-restaurant_pizzas.pizza']
+    # serialize_only = ('restaurant_pizzas.pizza')
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -48,11 +57,20 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
-
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
     # add relationships
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
 
     # add serialization rules
-
+    serialize_rules = ['-restaurant.restaurant_pizzas', 'pizza.restaurant_pizzas']
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 <= price <= 30):
+            raise ValueError("Price must be between 1 and 30.")
+        else:
+            return price
     # add validation
 
     def __repr__(self):
